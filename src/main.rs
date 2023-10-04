921ccff892078de29a15b81f5590ad775a8fb40a
ist1100120 <alexandre.f.coelho@tecnico.ulisboa.pt>
2023-10-04 02:05:13 +0100

    feat: pest integration && started grammar

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+extern crate pest;
+#[macro_use]
+extern crate pest_derive; 
+
+use pest::Parser;
+
+#[derive(Parser)]
+#[grammar = "parser.pest"]
+pub struct AvantParser;
+
+const CODE:&str="code;";
+
 fn main() {
     println!("Hello, world!");
+    let parse = AvantParser::parse(Rule::document, CODE)
+        .expect("unsuccessful parse") // unwrap the parse result
+        .next().unwrap(); // get and unwrap the `file` rule; never fails
+
+    println!("{:?}", parse);
 }
